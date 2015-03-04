b76c2fdae7826c21bc526e89ab2302bddb5fc55f
Johan Lindfors <johan.lindfors@coderox.se>
2015-03-04 21:53:39 +0100

    Cleaning up

--- CatapultWars/CatapultWars.Shared/Projectile.h
+++ CatapultWars/CatapultWars.Shared/Projectile.h
@@ -10,26 +10,26 @@ namespace CatapultWars {
 		void Fire(float velocityX, float velocityY);
 		bool UpdateProjectileFlightData(float wind, float gravity);
 
-		DirectX::SimpleMath::Vector2								ProjectilePosition;
-		DirectX::SimpleMath::Vector2								ProjectileStartPosition;
-		DirectX::SimpleMath::Vector2								ProjectileHitPosition;
-		float								ProjectileTextureWidth;
-		float								ProjectileTextureHeight;
+		DirectX::SimpleMath::Vector2						ProjectilePosition;
+		DirectX::SimpleMath::Vector2						ProjectileStartPosition;
+		DirectX::SimpleMath::Vector2						ProjectileHitPosition;
+		float												ProjectileTextureWidth;
+		float												ProjectileTextureHeight;
 		Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>	ProjectileTexture;
 
 	private:
-		Platform::String^					m_textureName;
-		DirectX::SimpleMath::Vector2		m_projectileVelocity;
-		float								m_projectileInitialVelocityY;
-		DirectX::SimpleMath::Vector2		m_projectileRotationPosition;
-		float								m_projectileRotation;
-		float								m_flightTime;
-		bool								m_isAI;
-		float								m_hitOffset;
-		float								m_gravity;
+		Platform::String^						m_textureName;
+		DirectX::SimpleMath::Vector2			m_projectileVelocity;
+		float									m_projectileInitialVelocityY;
+		DirectX::SimpleMath::Vector2			m_projectileRotationPosition;
+		float									m_projectileRotation;
+		float									m_flightTime;
+		bool									m_isAI;
+		float									m_hitOffset;
+		float									m_gravity;
 		std::shared_ptr<DirectX::SpriteBatch>	m_spriteBatch;
-		UINT								m_textureWidth;
-		UINT								m_textureHeight;
+		UINT									m_textureWidth;
+		UINT									m_textureHeight;
 	};
 }
 
--- README.md
+++ README.md
@@ -2,4 +2,4 @@ CatapultWars
 ============
 
 This is a C++/DirectX port of the XNA Catapult Wars tutorial available from MSDN. It's leveraging DirectXTK and tries to mimic the project structure as much as possible.
-It's a pure Windows Phone version currently.
+This is an universal solution with both a Windows and a Windows Phone solution. The rendering is messed up currently!
