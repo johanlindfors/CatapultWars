cb1fe906a07c3a6b1beb4a0a0194a612e10dc581
Johan Lindfors <Johan.Lindfors@coderox.se>
2015-03-05 20:22:40 +0100

    Cleaning up

--- CatapultWars/CatapultWars.Shared/CatapultWarsMain.cpp
+++ CatapultWars/CatapultWars.Shared/CatapultWarsMain.cpp
@@ -22,8 +22,8 @@ m_maxWind(2)
 
 	// TODO: Change the timer settings if you want something other than the default variable timestep mode.
 	// e.g. for 60 FPS fixed timestep update logic, call:
-	m_timer.SetFixedTimeStep(true);
-	m_timer.SetTargetElapsedSeconds(1.0 / 60);
+	//m_timer.SetFixedTimeStep(true);
+	//m_timer.SetTargetElapsedSeconds(1.0 / 60);
 
 	CreateWindowSizeDependentResources();
 }
